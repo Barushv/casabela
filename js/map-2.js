fd9baaff0adfa296af9bca32f17c504391497407
Barushv <fortiz.hondamontejo@gmail.com>
2023-10-26 00:01:28 -0600

    actualizacion a todo el sitio web

--- js/map-2.js
+++ js/map-2.js
@@ -1,33 +1,19 @@
 function initialize() {
-      var mapOptions = {
+  var mapOptions = {
     zoom: 13,
     scrollwheel: false,
-    center: new google.maps.LatLng(40.740610, -73.935242)
+    center: new google.maps.LatLng(21.06307269429664, -89.644172102349782),
   };
 
-  var map = new google.maps.Map(document.getElementById('googleMap-2'),
-      mapOptions);
-
+  var map = new google.maps.Map(
+    document.getElementById("googleMap-2"),
+    mapOptions
+  );
 
   var marker = new google.maps.Marker({
     position: map.getCenter(),
-    icon: 'img/map-marker-2.png',
-    map: map
+    icon: "img/map-marker-2.png",
+    map: map,
   });
-
 }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+google.maps.event.addDomListener(window, "load", initialize);
--- js/map.js
+++ js/map.js
@@ -1,47 +1,19 @@
 function initialize() {
-      var mapOptions = {
+  var mapOptions = {
     zoom: 10,
     scrollwheel: false,
-    center: new google.maps.LatLng(40.740610, -73.935242)
+    center: new google.maps.LatLng(21.06307269429664, -89.64417210234978),
   };
 
-  var map = new google.maps.Map(document.getElementById('googleMap'),
-      mapOptions);
-
+  var map = new google.maps.Map(
+    document.getElementById("googleMap"),
+    mapOptions
+  );
 
   var marker = new google.maps.Marker({
     position: map.getCenter(),
-    icon: 'img/map-marker.png',
-    map: map
+    icon: "img/map-marker.png",
+    map: map,
   });
-
 }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+google.maps.event.addDomListener(window, "load", initialize);
